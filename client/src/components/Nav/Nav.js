c2c696f3c030744d6a81961c0bb546c89bd14341
Duvanchi <cochize0@gmail.com>
2018-03-13 15:21:49 -0400

    stash for merge

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -5,10 +5,8 @@ const Nav = () =>
     <nav className="navbar navbar-inverse navbar-top">
         <div className="container-fluid">
             <div className="navbar-header">
-                <button type="button" className="collapsed navbar-toggle">
-                    <span className="icon-bar" />logo here <span className="icon-bar" />
-                    <span className="icon-bar" />
-                </button>
+                <img src="hotplate-logo-circle.png"></img>
+                <span className="icon-bar" />
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><a href="">Profile</a></li>
                     <li><a href="">About</a></li>
